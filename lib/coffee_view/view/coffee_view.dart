d24972e21d32964800356d8250815dddeef8fe81
Greg (Tommy) Thompson <gthompson333@gmail.com>
2024-10-12 21:55:14 -0700

    Added an app bar refresh button to the coffee view to enable fetching of a new coffee image.

--- lib/coffee_view/view/coffee_view.dart
+++ lib/coffee_view/view/coffee_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:very_good_coffee_app/coffee_view/bloc/coffee_image_cubit.dart';
+import 'package:very_good_coffee_app/constants.dart';
 
 class CoffeeView extends StatelessWidget {
   const CoffeeView({super.key});
@@ -8,27 +9,47 @@ class CoffeeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: const Text('Very Good Coffee App'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<CoffeeImageCubit>().fetchCoffeeImage();
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
       body: BlocBuilder<CoffeeImageCubit, CoffeeState>(
         builder: (context, state) {
           switch (state.coffeeStatus) {
             case CoffeeStatus.success:
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width - (40),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(3, 3),
-                      blurRadius: 5,
+              return Column(
+                children: [
+                  verticalSpace60,
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(3, 3),
+                            blurRadius: 5,
+                          ),
+                        ],
+                        image: DecorationImage(
+                          image: NetworkImage(state.coffeeData!.file),
+                        ),
+                      ),
                     ),
-                  ],
-                  image: DecorationImage(
-                    image: NetworkImage(state.coffeeData!.file),
                   ),
-                ),
+                ],
               );
             case CoffeeStatus.inProgress:
               return const Center(
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const verticalSpace20 = SizedBox(height: 20);
+const verticalSpace40 = SizedBox(height: 40);
+const verticalSpace60 = SizedBox(height: 60);
